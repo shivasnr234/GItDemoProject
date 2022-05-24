d4de72b98c16f58ebfb425539aab2eaf47898274
2062925 <2062925@LTIN329912.cts.com>
2022-05-24 09:34:40 +0530

    first commit..

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String rain="yes";
+		if(rain=="yes") {
+			System.out.println("so cool..");
+		}
+		else {
+			System.out.println("so hot..");
+			
+		}
+		
+		
+	}
+
+}
